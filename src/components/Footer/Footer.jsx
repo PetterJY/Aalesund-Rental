544ff05da956b49de01b62a1ffca5eeaee85ec4f
Mathias <mathiaslovnes02@gmail.com>
2025-03-07 13:41:13 +0100

    Footer now stacks vertically for small screens with a caret next to the menu-headers to indicate dropdown-menu.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Footer.css';
+import caretDown from "../../resources/images/caret-down.png";
 
 const Footer = () => {
   return (
     <footer>
 		<nav id="contact">
-			<button className="menu-button">Contact</button>
+			<div class="menu">
+				<p>Contact</p>
+				<img src={caretDown} alt="dropdown-menu-contact"/>
+			</div>
 			<address>
 				<ul className="footer-item">
 					<li><a href="mailto:Ålesund@rental.com">Ålesund@rental.com</a></li>
@@ -13,19 +17,23 @@ const Footer = () => {
 			</address>
 		</nav>
 
-		<nav id="links">
-			<div id="menu">
-				<h3>Menu</h3>
-				<img src="../../resources/images/caret-down.png" alt="dropdown-menu"></img>
-			</div>
-			<button className="menu-button">Links</button>
-			<ul className="footer-item">
-				<li><a href="https://github.com/PetterJY/Aalesund-Rental">GitHub</a></li>
-			</ul>
-		</nav>
+			<nav id="links">
+				<div class="menu">
+					<p>Links</p>
+					<img src={caretDown} alt="dropdown-menu-links"/>
+				</div>
+				<ul className="footer-item">
+					<li>
+						<a href="https://github.com/PetterJY/Aalesund-Rental">GitHub</a>
+					</li>
+				</ul>
+			</nav>
 
 		<nav id="creators">
-			<button className="menu-button">Creators</button>
+			<div class="menu">
+				<p>Creators</p>
+				<img src={caretDown} alt="dropdown-menu-creators"/>
+			</div>
 			<ul className="footer-item">
 				<li>Mathias Løvnes</li>
 				<li>Marcus Skaue</li>
