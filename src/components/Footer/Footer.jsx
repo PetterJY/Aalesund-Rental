5bc7e83b43ec8434fce08392d38d542f9b6cd8fe
Petter J. Ytterdahl <petterjy@hotmail.no>
2025-03-10 11:23:53 +0100

    Refactored the html and css for the logibutton into their own files. Created RegisterButton, jsx and css, files.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -53,7 +53,7 @@ const DropdownMenu = () => {
 
 	return (
 		<img src={caretDown}
-				 className={`dropdown ${isClicked ? 'clicked' : ''}`}
+				 className={`dropdown ${isExpanded ? 'expanded' : ''}`}
 				 onClick={handleDropDownMenuClick}
 				 alt="dropdown-menu-icon"/>
 	)
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import LoginButton from '../LoginButton/LoginButton';
 import logo from '../../resources/images/logo.png';
-import loginImage from '../../resources/images/login.png';
+import '../global.css';
 import './Header.css';
 
 const Header = () => {
@@ -17,26 +18,7 @@ const Header = () => {
   return (
     <header className="top-header">
       <img src={logo} id="logo-image" alt="Logo" />
-      <nav className="nav-bar">
-        <img src={loginImage} id="login-image" alt="Login" />
-        <button id="login-create" onClick={showModal}>Login|Register</button>
-      </nav>
-      {isModalVisible && (
-        <div id="loginModal" className="modal" onMouseDown={closeModal}>
-          <div className="modal-content" onMouseDown={(e) => e.stopPropagation()}>
-            <h2>Login</h2>
-            <form>
-              <input id="email-field" type="text" placeholder="E-mail" required />
-              <input id="password-field" type="password" placeholder="Password" required />
-              <button id="submit-button" type="submit">Login</button>
-            </form>
-            <section id="register-forgot-wrapper">
-              <button id="register-button">Create account</button>
-              <button id="forgot-password-button">Forgot password</button>
-            </section>
-          </div>
-        </div>
-      )}
+      <LoginButton showModal={showModal} closeModal={closeModal} isModalVisible={isModalVisible} />
     </header>
   );
 };
