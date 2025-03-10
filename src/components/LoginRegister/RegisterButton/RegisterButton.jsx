6fcdd72545ec6d90dfb09c7bc1280a9472b1ce37
Petter J. Ytterdahl <petterjy@hotmail.no>
2025-03-10 14:44:29 +0100

    Refactored LoginButton and RegisterButton to use the same css file. Fixed alignment in RegisterButton.

---
+++ src/components/LoginRegister/RegisterButton/RegisterButton.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../../global.css';
+import '../LoginRegister.css';
+
+const RegisterButton = ({ closeModal, isModalVisible, toggleMode }) => {
+  return (
+    <>
+      {isModalVisible && (
+        <div id="registerModal" className="modal" onMouseDown={closeModal}>
+          <div className="modal-content" onMouseDown={(e) => e.stopPropagation()}>
+            <h2 id="title">Create Account</h2>
+            <form id="wrapper">
+              <input id="email-field" type="text" placeholder="E-mail" required />
+              <div id="name-wrapper">
+                <input id="first-name-field" type="text" placeholder="First Name" required />
+                <input id="last-name-field" type="text" placeholder="Last Name" required />
+              </div>
+              <div id="password-wrapper">
+                <input id="password-field" type="password" placeholder="Password" required />
+                <input id="confirm-password-field" type="password" placeholder="Confirm Password" required />
+              </div>
+              <button id="submit-button" type="submit">Register</button>
+            </form> 
+            <button class="toggle-login-register-button" onClick={toggleMode}>Already have an account? Login</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default RegisterButton;
