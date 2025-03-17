dcf629156521dbcbd989f93170e98b9d59a04ee4
Mathias <mathiaslovnes02@gmail.com>
2025-03-17 13:12:55 +0100

    Changed javascript structure for the home page to be more modular - Now label is created outside of the DatePicker method.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,52 +1,58 @@
 import "../global.css";
 import './Home.css';
 import React from "react";
-import calenderImage from "../../resources/images/calendar.png"
+import calenderImage from "../../resources/images/calendar.png";
 
 const Home = (props) => {
+
+  const pickupSection = (
+    <div className="pickup-dropoff">
+      <label id="pickup-label" className="pickup-dropoff-text">Pickup</label>
+      <DatePicker date={props.pickUpDate} time={props.pickUpTime} />
+    </div>
+  );
+
+  const dropoffSection = (
+    <div className="pickup-dropoff">
+      <label className="dropoff-label pickup-dropoff-text">Dropoff</label>
+      <DatePicker date={props.dropOffDate} time={props.dropOffTime} />
+    </div>
+  );
+
+  const showCarsSection = (
+    <div className="show-cars">
+      <button className="show-cars-button">Show cars</button>
+    </div>
+  );
+
   return (
     <main id="content">
       <section id="styledContainer">
-        <DatePicker
-            label="Pickup"
-            date={props.pickUpDate}
-            time={props.pickUpTime}
-        />
-        <DatePicker
-          label="Dropoff"
-          date={props.dropOffDate}
-          time={props.dropOffTime}
-        />
-        <div class="show-cars">
-          <button class="show-cars-button">
-            Show cars
-          </button>
-        </div>
+        {pickupSection}
+        {dropoffSection}
+        {showCarsSection}
       </section>
     </main>
   );
 };
 
-const DatePicker = ({label, date, time}) => {
+const DatePicker = ({ date, time }) => {
   return (
-      <section className="pickup-dropoff">
-        <label className="pickup-dropoff-text">{label}</label>
-        <div className="date-time-picker">
-          <div className="date-time-picker-content">
-            <div className="date-picker-content">
-              <button className="date-picker-button"></button>
-              <img className="calendar-image" src={calenderImage} alt="calendar image"/>
-              <p>{date}</p>
-            </div>
-            <div className="divider"></div>
-            <div className="time-picker-content">
-              <button className="time-picker-button"></button>
-              <p>{time}</p>
-            </div>
-          </div>
+    <div className="date-time-picker">
+      <div className="date-time-picker-content">
+        <div className="date-picker-content">
+          <button className="date-picker-button"></button>
+          <img className="calendar-image" src={calenderImage} alt="calendar image" />
+          <p>{date}</p>
         </div>
-      </section>
-  )
-}
+        <div className="divider"></div>
+        <div className="time-picker-content">
+          <button className="time-picker-button"></button>
+          <p>{time}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
