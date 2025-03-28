3df69a7fed95ece6244ac07eae46a21c7949749b
Skaue <marcus@skaue.com>
2025-03-28 14:17:08 +0100

    Added REST-API mapping for the CarsController and ExtraFeaturesController.

--- src/main/java/no/ntnu/logic/controller/CarsController.java
+++ src/main/java/no/ntnu/logic/controller/CarsController.java
@@ -1,12 +1,71 @@
 package no.ntnu.logic.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import no.ntnu.entity.Cars;
 import no.ntnu.logic.repository.CarsRepository;
 
 @RestController
+@RequestMapping("/cars")
 public class CarsController {
   @Autowired
-  private CarsRepository carsRepository;
+  private final CarsRepository carsRepository;
+
+  public CarsController(CarsRepository carsRepository) {
+    this.carsRepository = carsRepository;
+  }
+
+  @GetMapping
+  public List<Cars> getAllCars() {
+    return (List<Cars>) carsRepository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Cars> getCarById(@PathVariable Long id) {
+    Optional<Cars> car = carsRepository.findById(id);
+    return car.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+
+  @PostMapping
+  public Cars createCar(@RequestBody Cars car) {
+    return carsRepository.save(car);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Cars> updateCar(@PathVariable Long id, @RequestBody Cars carDetails) {
+    Optional<Cars> car = carsRepository.findById(id);
+    if (car.isPresent()) {
+      Cars updatedCar = car.get();
+      updatedCar.setPlateNumber(carDetails.getPlateNumber());
+      updatedCar.setCarBrand(carDetails.getCarBrand());
+      updatedCar.setModelName(carDetails.getModelName());
+      updatedCar.setCarType(carDetails.getCarType());
+      updatedCar.setPricePerDay(carDetails.getPricePerDay());
+      updatedCar.setProductionYear(carDetails.getProductionYear());
+      updatedCar.setPassengers(carDetails.getPassengers());
+      updatedCar.setAutomatic(carDetails.isAutomatic());
+      updatedCar.setEnergySource(carDetails.getEnergySource());
+      updatedCar.setAvailable(carDetails.isAvailable());
+      updatedCar.setExtraFeatures(carDetails.getExtraFeatures());
+      return ResponseEntity.ok(carsRepository.save(updatedCar));
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteCar(@PathVariable Long id) {
+    if (carsRepository.existsById(id)) {
+      carsRepository.deleteById(id);
+      return ResponseEntity.noContent().build();
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
 }
--- src/main/java/no/ntnu/logic/controller/ExtraFeaturesController.java
+++ src/main/java/no/ntnu/logic/controller/ExtraFeaturesController.java
@@ -1,12 +1,62 @@
 package no.ntnu.logic.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import no.ntnu.entity.ExtraFeatures;
 import no.ntnu.logic.repository.ExtraFeaturesRepository;
 
 @RestController
+@RequestMapping("/extra-features")
 public class ExtraFeaturesController {
   @Autowired
-  private ExtraFeaturesRepository extraFeaturesRepository;
+  private final ExtraFeaturesRepository extraFeaturesRepository;
+
+  public ExtraFeaturesController(ExtraFeaturesRepository extraFeaturesRepository) {
+    this.extraFeaturesRepository = extraFeaturesRepository;
+  }
+
+  @GetMapping
+  public List<ExtraFeatures> getAllExtraFeatures() {
+    return (List<ExtraFeatures>) extraFeaturesRepository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<ExtraFeatures> getExtraFeatureById(@PathVariable long id) {
+    Optional<ExtraFeatures> extraFeature = extraFeaturesRepository.findById(id);
+    return extraFeature.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @PostMapping
+  public ExtraFeatures createExtraFeature(@RequestBody ExtraFeatures extraFeature) {
+    return extraFeaturesRepository.save(extraFeature);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<ExtraFeatures> updateExtraFeature(@PathVariable long id, @RequestBody ExtraFeatures extraFeatureDetails) {
+    Optional<ExtraFeatures> extraFeature = extraFeaturesRepository.findById(id);
+    if (extraFeature.isPresent()) {
+      ExtraFeatures updatedExtraFeature = extraFeature.get();
+      updatedExtraFeature.setName(extraFeatureDetails.getName());
+      updatedExtraFeature.setDescription(extraFeatureDetails.getDescription());
+      updatedExtraFeature.setCars(extraFeatureDetails.getCars());
+      return ResponseEntity.ok(extraFeaturesRepository.save(updatedExtraFeature));
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteExtraFeature(@PathVariable long id) {
+    if (extraFeaturesRepository.existsById(id)) {
+      extraFeaturesRepository.deleteById(id);
+      return ResponseEntity.noContent().build();
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
 }
