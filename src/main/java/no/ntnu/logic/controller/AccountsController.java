1f21b1e3bf9ca85c9415d512f37d8e73693e8bc6
Skaue <marcus@skaue.com>
2025-03-31 14:47:08 +0200

    Cleaned up some code.

--- src/main/java/no/ntnu/logic/controller/AccountsController.java
+++ src/main/java/no/ntnu/logic/controller/AccountsController.java
@@ -1,16 +1,15 @@
 package no.ntnu.logic.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import io.swagger.annotations.ApiOperation;
 import no.ntnu.entity.exceptions.AccountNotFoundException;
+import no.ntnu.entity.Accounts;
 import no.ntnu.logic.service.AccountsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,9 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.ApiOperation;
-import no.ntnu.entity.Accounts;
-import no.ntnu.logic.service.AccountsService;
 
 @RestController
 @RequestMapping("/accounts")
