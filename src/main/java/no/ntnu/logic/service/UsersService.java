888abda2b3029c9b368061022e601eaff1844890
Mathias <mathiaslovnes02@gmail.com>
2025-04-01 12:58:30 +0200

    Integrated UserService with GlobalExceptionHandler.

--- src/main/java/no/ntnu/logic/service/UsersService.java
+++ src/main/java/no/ntnu/logic/service/UsersService.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import no.ntnu.entity.Users;
@@ -25,48 +24,22 @@ public class UsersService {
   }
 
   public Iterable<Users> findAll() {
-    try {
-      return usersRepository.findAll();
-    } catch (DataAccessException e) {
-      logger.error("Error accessing data while fetching all users", e);
-      throw new DataAccessException("Unable to fetch users", e) {};
-    }
+    return usersRepository.findAll();
   }
 
   public Users findById(Long id) {
-    try {
-      return usersRepository.findById(id)
-        .orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
-    } catch (DataAccessException e) {
-      logger.error("Error accessing data while fetching user with id: {}", id, e);
-      throw new DataAccessException("Unable to fetch user with id: " + id, e) {};
-    }
+    return usersRepository.findById(id)
+      .orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
   }
 
   public Users save(Users user) {
-    try {
-      return usersRepository.save(user);
-    } catch (ConstraintViolationException e) {
-      logger.error("Validation error while saving user", e);
-      throw new DataAccessException("Validation error while saving user", e) {};
-    } catch (DataAccessException e) {
-      logger.error("Error accessing data while saving user with id {}", user.getId(), e);
-      throw new DataAccessException("Unable to save user", e) {};
-    }
+    return usersRepository.save(user);
   }
 
   public void deleteById(Long id) {
-    try {
-      if (!usersRepository.existsById(id)) {
-        throw new UserNotFoundException("User not found with id: " + id);
-      }
-      usersRepository.deleteById(id);
-    } catch (DataIntegrityViolationException e) {
-      logger.error("Integrity violation while deleting user with id: {}", id, e);
-      throw new DataAccessException("Integrity violation while deleting user with id: " + id, e) {};
-    } catch (DataAccessException e) {
-      logger.error("Error accessing data while deleting user with id: {}", id, e);
-      throw new DataAccessException("Unable to delete user with id: " + id, e) {};
+    if (!usersRepository.existsById(id)) {
+      throw new UserNotFoundException("User not found with id: " + id);
     }
+    usersRepository.deleteById(id);
   }
 }
