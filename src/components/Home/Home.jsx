ff5f3806a5cdc04fbd5e6c486432a1f27bc2ddd8
Mathias Løvnes <mathiaslovnes02@gmail.com>
2025-05-16 12:59:48 +0200

    Hero-call-to-action section on front page now does not appear in front of the footer.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, {useState, useEffect, useContext, useRef} from 'react';
 import { useNavigate } from 'react-router-dom';
 import BookingForm from './BookingForm/BookingForm';
 import Testimonials from "./Testimonials/Testimonials";
@@ -8,10 +8,12 @@ import CallToAction from "./CallToAction/CallToAction";
 import '../App.css';
 import './Home.css';
 import {BookingContext} from "../utils/BookingContext";
+import callToAction from "./CallToAction/CallToAction";
 
 const Home = () => {
   const navigate = useNavigate();
   const [mobileDisplaySize, setMobileDisplaySize] = useState(false);
+  const callToActionRef = useRef(null);
 
   const {bookingData} = useContext(BookingContext)
 
@@ -29,15 +31,35 @@ const Home = () => {
     navigate('/rental');
   };
 
+  useEffect(() => {
+    const callToAction = callToActionRef.current;
+    const triggerPoint = 500;
+
+    const handleScroll = () => {
+      if (window.scroll > triggerPoint) {
+        callToAction.style.position = 'absolute';
+        callToAction.style.top = `${triggerPoint}px`;
+      } else {
+        callToAction.style.position = 'fixed';
+        callToAction.style.top = '0';
+
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   const HeroCallToAction = () => {
     const [isRendered, setIsRendered] = useState(false);
+
 
     useEffect(() => {
       setIsRendered(true);
     }, []);
 
     return (
-      <div className="hero-call-to-action">
+      <div className="hero-call-to-action" ref={callToAction}>
         <h1 className={`${isRendered ? 'fade-and-drop-in' : ''}`}>Ready to Take a Trip?</h1>
         <h2 className={`${isRendered ? 'fade-in' : ''}`}>Our cars can take you anywhere, anytime.</h2>
       </div>
