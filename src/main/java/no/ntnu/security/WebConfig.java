4f8da72b2b94f1c4f7650e407d413e1680961e09
Petter J. Ytterdahl <petterjy@hotmail.no>
2025-05-21 21:17:20 +0200

    Updated the index.html file.

--- src/main/java/no/ntnu/security/WebConfig.java
+++ src/main/java/no/ntnu/security/WebConfig.java
@@ -10,17 +10,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins(
-									"http://localhost", 
-									"http://localhost:3000", 
-									"http://localhost:3001", 
-									"https://norwegian-rental.online") 
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true)
-            .maxAge(3600);
-    }
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins(
+            "http://localhost", 
+          "http://localhost:3000", 
+          "http://localhost:3001", 
+          "https://norwegian-rental.online") 
+    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+    .allowedHeaders("*")
+    .allowCredentials(true)
+    .maxAge(3600);
+  }
 }
